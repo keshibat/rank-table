4d11250434ec90c590cf36c9ebc155512b5028f7
keshibat <kensukeshibata@gmail.com>
2022-07-07 03:41:04 +0900

    generate random number

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import LiverList from './components/liver-list/liver-list.component';
 
 function App() {
   const [livers, serLivers] = useState([]);
-  
-
 
   useEffect(() => {
-    fetch('https://webcdn.17app.co/campaign/pretest/data.json')
-      .then((response) => response.json())
-      .then((data) => serLivers(data))
-  }, [])
-  console.log(livers);
+    const interval = setInterval(() => {
+      fetchLiverData();
+      console.log( getRandomPoint())
+    }, 3000);
+    return () => clearInterval(interval)
+  }, [livers])
+
+  const fetchLiverData = async () => {
+    const response = await fetch('https://webcdn.17app.co/campaign/pretest/data.json');
+    const data = await response.json();
+    const sortedData = data.sort((a,b) => b.score - a.score);
+    serLivers(sortedData);
+  }
+
+  const getRandomPoint = () => {
+    const MIN = 50;
+    const MAX = 10000;
+    let randomFloat = Math.random();
+    let randomInt = Math.floor(randomFloat * (MAX - MIN)) + MIN;
+    return randomInt
+  }
+
+  // useEffect(() => {
+  //   fetch('https://webcdn.17app.co/campaign/pretest/data.json')
+  //     .then((response) => response.json())
+  //     .then((data) => serLivers(data))
+  // }, [])
+
 
   return (
     <div>

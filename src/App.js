d1a0b817017e7d5675b8175ce2d380c590c5aee6
keshibat <kensukeshibata@gmail.com>
2022-07-07 20:05:00 +0900

    ftb

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import {
 } from 'react';
 import LiverList from './components/liver-list/liver-list.component';
 
+
+// {
+// "userID": "4f4d5462-4a9f-483e-b620-9df9c13ec840",
+// "displayName": "Jone",
+// "picture": "https://assets-17app.akamaized.net/THUMBNAIL_525BEE6E-94B5-4C7F-AB47-1A6F9735EE82.jpg",
+// "score": 157000
+// },
+// {
+// "userID": "2c0c5c67-1cfc-4b99-992e-2918b8a0dcc1",
+// "displayName": "Victoria",
+// "picture": "https://assets-17app.akamaized.net/THUMBNAIL_8390066D-46E4-4741-968D-9FF84B276B52.jpg",
+// "score": 46200
+// },
+
 function App() {
   const [livers, serLivers] = useState([]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       fetchLiverData();
-      console.log( getRandomPoint())
     }, 3000);
     return () => clearInterval(interval)
   }, [livers])
@@ -24,6 +37,14 @@ function App() {
   const getRandomPoint = () => {
     const MIN = 50;
     const MAX = 10000;
+    let randomFloat = Math.random();
+    let randomInt = Math.floor(randomFloat * (MAX - MIN)) + MIN;
+    return randomInt
+  }
+
+    const getRandomIndex = () => {
+    const MIN = 0;
+    const MAX = 10;
     let randomFloat = Math.random();
     let randomInt = Math.floor(randomFloat * (MAX - MIN)) + MIN;
     return randomInt

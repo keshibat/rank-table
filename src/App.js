6b7d66bd892579378105173bbe6e22ec9749bbde
keshibat <kensukeshibata@gmail.com>
2022-07-08 01:57:44 +0900

    added adding points logic

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import {
 } from 'react';
 import LiverList from './components/liver-list/liver-list.component';
 
-
-// {
-// "userID": "4f4d5462-4a9f-483e-b620-9df9c13ec840",
-// "displayName": "Jone",
-// "picture": "https://assets-17app.akamaized.net/THUMBNAIL_525BEE6E-94B5-4C7F-AB47-1A6F9735EE82.jpg",
-// "score": 157000
-// },
-// {
-// "userID": "2c0c5c67-1cfc-4b99-992e-2918b8a0dcc1",
-// "displayName": "Victoria",
-// "picture": "https://assets-17app.akamaized.net/THUMBNAIL_8390066D-46E4-4741-968D-9FF84B276B52.jpg",
-// "score": 46200
-// },
-
 function App() {
   const [livers, serLivers] = useState([]);
 
@@ -27,10 +13,16 @@ function App() {
     return () => clearInterval(interval)
   }, [livers])
 
+
   const fetchLiverData = async () => {
     const response = await fetch('https://webcdn.17app.co/campaign/pretest/data.json');
-    const data = await response.json();
-    const sortedData = data.sort((a,b) => b.score - a.score);
+    let data = await response.json();
+    let addPoints = getRandomPoint();
+    let userIndex = getRandomIndex();
+
+    let updateData = [...data]
+    updateData[userIndex].score += addPoints
+    const sortedData = updateData.sort((a,b) => b.score - a.score);
     serLivers(sortedData);
   }
 
@@ -49,13 +41,6 @@ function App() {
     let randomInt = Math.floor(randomFloat * (MAX - MIN)) + MIN;
     return randomInt
   }
-
-  // useEffect(() => {
-  //   fetch('https://webcdn.17app.co/campaign/pretest/data.json')
-  //     .then((response) => response.json())
-  //     .then((data) => serLivers(data))
-  // }, [])
-
 
   return (
     <div>
